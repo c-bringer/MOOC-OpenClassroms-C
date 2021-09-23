6624b066f6f9e5f0700d0ae35a0d7f4f79021a7f
Kermit <44336706+c-bringer@users.noreply.github.com>
2021-09-23 13:01:30 +0200

    TP - Plus ou moins

---
+++ PusOuMoins/PusOuMoins/main.c
@@ -0,0 +1,45 @@
+//
+//  main.c
+//  PusOuMoins
+//
+//  Created by Corentin Bringer on 23/09/2021.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, const char * argv[]) {
+    //Declaration variables
+    int nbRandom = 0;
+    int nbEntree = 0;
+    int nbCoup = 0;
+    const int nbMin = 1;
+    const int nbMax = 100;
+    
+    //Generation nombre aleatoire
+    srand(time(NULL));
+    nbRandom = (rand() % (nbMax - nbMin - 1)) + nbMin;
+
+    printf("Jeu du plus ou moins!\n");
+    
+    do {
+        //Saisi nb
+        printf("Quel est le nombre mystere ? <1..100>");
+        scanf("%d",&nbEntree);
+
+        nbCoup++;
+
+        //Affichage
+        if (nbEntree < nbRandom) {
+            printf("C'est plus!\n");
+        } else if (nbEntree > nbRandom) {
+            printf("C'est moins!\n");
+        } else {
+            printf("Vous avez trouve!\n");
+            printf("Nombre de coup: %d",nbCoup);
+        }
+    } while(nbEntree != nbRandom); //nbEntree différent de nbRandom
+
+    return 0;
+}
